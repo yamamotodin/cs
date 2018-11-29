b6487319c62907bf86d0ed981fd2953fe190ecf0
yamamotodin <ragnarock00>
2018-11-29 16:26:43 +0900

    create joint pdf.

--- convertPdf.sh
+++ convertPdf.sh
@@ -3,3 +3,4 @@
 npm install -g markdown-pdf
 markdown-pdf career_sheet.md -o career_sheet.pdf
 markdown-pdf career_sheet_2.md -o career_sheet_2.pdf
+markdown-pdf career_sheet.md career_sheet_2.md -o career_sheet_joint.pdf
