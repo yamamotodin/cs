59630205e5d50a74ae420cd2cb23213fe5228731
yamamotodin <yamamotodin@gmail.com>
2026-07-30 19:19:17 +0900

    actionsの修正

--- convertPdf.sh
+++ convertPdf.sh
@@ -7,8 +7,8 @@ fi
 
 
 npm install -g md-to-pdf
-md-to-pdf career_sheet.md
-md-to-pdf career_sheet_2.md
+md-to-pdf career_sheet.md --launch-options '{"args": ["--no-sandbox", "--disable-setuid-sandbox"]}'
+md-to-pdf career_sheet_2.md --launch-options '{"args": ["--no-sandbox", "--disable-setuid-sandbox"]}'
 cat career_sheet.md career_sheet_2.md > career_sheet_joint.md
-md-to-pdf career_sheet_joint.md
+md-to-pdf career_sheet_joint.md --launch-options '{"args": ["--no-sandbox", "--disable-setuid-sandbox"]}'
 rm career_sheet_joint.md
